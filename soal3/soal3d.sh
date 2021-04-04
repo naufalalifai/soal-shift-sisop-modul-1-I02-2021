06e19dd6d2f64e1e53a9d6a25151f0814d68b9c3
naufalalifai <73649094+naufalalifai@users.noreply.github.com>
2021-04-04 14:03:09 +0700

    Update soal3d.sh

--- soal3/soal3d.sh
+++ soal3/soal3d.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
 
+cd /home/alifai/Documents/Shift1
+
+if [ $(date +%H%M) == '0700' ]
+then
+	zip -rm -P $(date '+%m%d%Y') Koleksi.zip Kucing_* Kelinci_*
+elif [ $(date +%H%M) == '1800' ]
+then
+	unzip -P $(date '+%m%d%Y') Koleksi.zip 
+	rm Koleksi.zip
+fi
